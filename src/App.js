16209e7a377ab85d8b312a01f36f59eddd8f66ee
klemenslatnar <klemen.slatnar06@gmail.com>
2022-08-15 12:57:18 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useRef } from "react";
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const inputRef = useRef();
+
+  const removeHandler = (id) => {
+    setToDoList((prevState) => {
+      return [
+        ...prevState.filter((el) => {
+          return el.id !== id;
+        }),
+      ];
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredInput = inputRef.current.value;
+
+    if (enteredInput === "") {
+      alert("Enter A Value");
+    } else {
+      setToDoList((prevState) => [
+        ...prevState,
+        { text: enteredInput, id: Math.trunc(Math.random() * 100000) },
+      ]);
+      inputRef.current.value = "";
+    }
+  };
+
+  console.log(toDoList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <div className="box">
+          <form onSubmit={submitHandler} className="input">
+            <h1>To Do List</h1>
+            <input ref={inputRef} type="text" />
+            <button className="btn-add">Add</button>
+          </form>
+          <div className="to-do">
+            {toDoList.map((toDo) => {
+              return (
+                <div style={{ margin: "20px", textAlign: "right" }}>
+                  <span style={{ marginRight: "10px" }}>{toDo.text}</span>
+                  <button
+                    className="btn-check"
+                    onClick={() => removeHandler(toDo.id)}
+                  >
+                    ✔
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
